2485c7ee968b4770d3cbe213e7f65d5368a4a062
Tharun <Tharun@DESKTOP-BTLNLM5>
2018-05-13 11:27:04 +0530

    next version

--- JenkinsAutorun/src/FirstJava.java
+++ JenkinsAutorun/src/FirstJava.java
@@ -3,7 +3,7 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Jenkins Autorun changed welcome");
+		System.out.println("Jenkins Autorun changed");
 	}
 
 }
